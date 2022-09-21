6abeca692d60c0ca677336d729200c94b901c559
Mohammad Wasikuzzaman Azad Sajol <wasik0003@gmail.com>
2022-09-21 13:41:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express');
-
 const request = require('request-promise');
-
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 
 createScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
-
 app.get('/', (req, res) => {
     res.send('Welcome to Daraz Scraper API!');
 });
-
 
 // Getting Product Details
 
